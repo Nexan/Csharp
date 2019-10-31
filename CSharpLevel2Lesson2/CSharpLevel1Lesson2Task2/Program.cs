0f0af8fba7863bcf54a4d1d615b851a3d5ac5279
Nexan <nexan@bk.ru>
2019-10-31 21:46:38 +0500

    C# урок 2 задания

---
+++ CSharpLevel2Lesson2/CSharpLevel1Lesson2Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Михеев Константин
+    Задание 2:
+        Написать метод подсчета количества цифр числа. 
+ */
+using System;
+
+namespace CSharpLevel1Lesson2Task2
+{
+    class Program
+    {
+        static int CountOfNumbers(int number)
+        {
+            int count = 0;
+
+            while (number != 0)
+            {
+                number /= 10;
+                count++;
+            }
+
+            return count;
+        }
+
+        static string Declension(int number)
+        {
+            string result = "цифр";
+            int num10 = number % 10;
+            int num100 = number % 100;
+
+            if (num10 == 1 && num100 != 11)
+            {
+                result = "цифра";
+            } else
+            {
+                if ((num10 > 1 && num10 < 5) && (num100 < 12 || num100 > 14))
+                {
+                    result = "цифры";
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int res = CountOfNumbers(n);
+
+            Console.WriteLine($"В числе {n} содержится {res} {Declension(res)}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpLevel2Lesson2/CSharpLevel1Lesson2Task5/Program.cs
@@ -0,0 +1,128 @@
+﻿/* Михеев Константин
+    Задание 5:
+        а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс 
+        массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме. 
+        б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса. 
+*/
+using System;
+
+namespace CSharpLevel1Lesson2Task5
+{
+    class Program
+    {
+        static double WeightIndex(double height, double weight)
+        {
+            return weight / (height * height);
+        }
+
+        static string WeightAlert(double weightIndex)
+        {
+            string result;
+
+            if (weightIndex <= 16)
+            {
+                result = "У вас выраженный дефицит массы тела";
+            }
+            else
+            {
+                if (weightIndex > 16 && weightIndex <= 18.5)
+                {
+                    result = "У вас недостаточная масса тела";
+                }
+                else
+                {
+                    if (weightIndex > 18.5 && weightIndex < 25)
+                    {
+                        result = "У вас нормальная масса тела";
+                    }
+                    else
+                    {
+                        if (weightIndex >= 25 && weightIndex < 30)
+                        {
+                            result = "У вас избыточная масса тела (предожирение)";
+                        }
+                        else
+                        {
+                            if (weightIndex >= 30 && weightIndex < 35)
+                            {
+                                result = "У вас ожирение";
+                            }
+                            else
+                            {
+                                if (weightIndex >= 35 && weightIndex < 40)
+                                {
+                                    result = "У вас резкое ожирение";
+                                }
+                                else
+                                {
+                                    result = "У вас очень резкое ожирение";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static double WeightRecommendation(double height, double weight)
+        {
+            double weightIndex = WeightIndex(height, weight);
+
+            if (weightIndex <= 18.5)
+            {
+                return 18.6 * height * height;
+            }
+            else
+            {
+                if (weightIndex >= 25)
+                {
+                    return 24.99 * height * height;
+                }
+                else
+                {
+                    return weight;
+                }
+            }
+        }
+
+        static string WeightRecommendationAlert(double height, double weight)
+        {
+            double recommendedWeight = WeightRecommendation(height, weight);
+
+            if (weight < recommendedWeight)
+            {
+                return string.Format("Вам необходимо набарть минимум {0:f3} кг", recommendedWeight - weight);
+            }
+            else
+            {
+                if (weight > recommendedWeight)
+                {
+                    return string.Format("Вам необходимо сбросить минимум {0:f3} кг", weight - recommendedWeight);
+                }
+            }
+
+            return "Вам ничего менять не надо";
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваш вес в кг: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите ваш рост в метрах (например, 1.75 или 1,75): ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double weightIndex = WeightIndex(height, weight);
+
+            Console.Clear();
+            Console.WriteLine($"Для веса {weight} кг и роста {height} м, индекс массы тела равен {weightIndex:F0}");
+            Console.WriteLine($"{WeightAlert(weightIndex)}");
+            Console.WriteLine(WeightRecommendationAlert(height, weight));
+
+            Console.ReadKey();
+        }
+    }
+}
