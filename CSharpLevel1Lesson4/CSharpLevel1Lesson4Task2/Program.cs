5fc9809ebb5bbe7dd53f0089d318f3b3d02a8af9
Nexan <nexan@bk.ru>
2019-11-07 21:22:58 +0500

    Задание 4-го урока курса C# (уровень 1)

---
+++ CSharpLevel1Lesson4/CSharpLevel1Lesson4Task2/Program.cs
@@ -0,0 +1,150 @@
+﻿/* Михеев Константин
+    Задание 2:
+        Реализуйте задачу 1 в виде статического класса StaticClass; 
+        а)  Класс  должен  содержать  статический  метод,  который  принимает на вход массив и решает 
+        задачу 1; 
+        б)  *Добавьте  статический  метод для считывания массива из текстового файла. Метод должен 
+        возвращать массив целых чисел; 
+        в)**Добавьте обработку ситуации отсутствия файла на диске. 
+ */
+using System;
+using System.IO;
+
+namespace CSharpLevel1Lesson4Task2
+{
+    static class StaticClass
+    {
+        /// <summary>
+        /// Создает массив размерности arrayLength и заполняет его случайными числами в интервале от -10000 до 10000.
+        /// </summary>
+        /// <param name="arrayLength"></param>
+        /// <returns></returns>
+        public static int[] CreateArray(int arrayLength)
+        {
+            int[] tmp = new int[arrayLength];
+            Random rnd = new Random();
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                tmp[i] = rnd.Next(-10000, 10001);
+            }
+
+            return tmp;
+        }
+
+        /// <summary>
+        /// Считывает из файла filename числовые данные и создает массив.
+        /// Файла должен содержать целые числа. В первой строке содержится количество элементов массива.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static int[] CreateArrayFromFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                throw new Exception($"Файл {filename} не найден.");
+            }
+
+            StreamReader reader = new StreamReader(filename);
+            int arrayLength = Convert.ToInt32(reader.ReadLine());
+
+            int[] tmp = new int[arrayLength];
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                tmp[i] = Convert.ToInt32(reader.ReadLine());
+            }
+
+            reader.Close();
+
+            return tmp;
+        }
+
+        /// <summary>
+        /// Находит пары элеметов в массиве tmp, в которых только один элемент делится на 3.
+        /// А также расчитывает количество таких пар и записывает их в pairsCount.
+        /// </summary>
+        /// <param name="tmp"></param>
+        /// <param name="pairsCount"></param>
+        /// <returns></returns>
+        public static int[,] Pairs(int[] tmp, out int pairsCount)
+        {
+            int length = tmp.Length;
+            int[,] pairs = new int[length, 2];
+            pairsCount = 0;
+
+            for (int i = 1; i < length; i++)
+            {
+                if ((tmp[i - 1] % 3 == 0 && tmp[i] % 3 != 0)
+                    || (tmp[i - 1] % 3 != 0 && tmp[i] % 3 == 0))
+                {
+                    pairs[pairsCount, 0] = i - 1;
+                    pairs[pairsCount, 1] = i;
+                    pairsCount++;
+                }
+            }
+
+            return pairs;
+        }
+
+        /// <summary>
+        /// Печатает все элементы массива tmp в одну строку.
+        /// </summary>
+        /// <param name="tmp"></param>
+        public static void PrintAll(int[] tmp)
+        {
+            foreach (int i in tmp)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        /// <summary>
+        /// Печатает пары элементов из массива tmp. Индексы пар содержатся в массиве pair, а количество пар в pairsCount.
+        /// </summary>
+        /// <param name="tmp"></param>
+        /// <param name="pairs"></param>
+        /// <param name="pairsCount"></param>
+        public static void PrintPairs(int[] tmp, int[,] pairs, int pairsCount)
+        {
+            for (int i = 0; i < pairsCount; i++)
+            {
+                Console.WriteLine($"{tmp[pairs[i, 0]],7} {tmp[pairs[i, 1]],7}");
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+
+            // массив со случайными числами
+            int[] arr = StaticClass.CreateArray(30);
+            int[,] pairs = StaticClass.Pairs(arr, out count);
+            Console.WriteLine("Пример создания массива из 30 элементов, заполненного случайными числами.\n");
+            Console.WriteLine("Печать всех элементов массива:");
+            StaticClass.PrintAll(arr);
+            Console.WriteLine($"\n\nКоличество пар, только с одним делящимся на 3 элементом: {count}");
+            Console.WriteLine("Печать пар элементов:");
+            StaticClass.PrintPairs(arr, pairs, count);
+            Console.ReadKey();
+
+            // массив из файла
+            arr = StaticClass.CreateArrayFromFile("data.txt");
+            pairs = StaticClass.Pairs(arr, out count);
+
+            Console.Clear();
+            Console.WriteLine("Пример создания массива из файла data.txt.\n");
+            Console.WriteLine("Печать всех элементов массива:");
+            StaticClass.PrintAll(arr);
+            Console.WriteLine($"\n\nКоличество пар, только с одним делящимся на 3 элементом: {count}");
+            Console.WriteLine("Печать пар элементов:");
+            StaticClass.PrintPairs(arr, pairs, count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpLevel1Lesson4/CSharpLevel1Lesson4Task5/Program.cs
@@ -0,0 +1,76 @@
+﻿/* Михеев Константин
+    Задание 5:
+        *а)  Реализовать  библиотеку  с  классом  для  работы  с  двумерным  массивом.  Реализовать 
+        конструктор, заполняющий массив случайными числами. Создать методы, которые возвращают 
+        сумму  всех  элементов  массива,  сумму  всех  элементов  массива  больше  заданного,  свойство, 
+        возвращающее  минимальный  элемент  массива,  свойство,  возвращающее  максимальный 
+        элемент  массива,  метод,  возвращающий  номер  максимального  элемента  массива  (через 
+        параметры, используя модификатор ref или out). 
+        **б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные 
+        в файл. 
+        **в) Обработать возможные исключительные ситуации при работе с файлами. 
+ */
+using System;
+using TwoRangeArray;
+
+namespace CSharpLevel1Lesson4Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтение массива из файла data1.txt");
+            Array2D arr = new Array2D("data1.txt");
+            arr.Print();
+            Console.WriteLine($"Минимальный элемент массива равен {arr.Min}");
+            Console.WriteLine($"Максимальный элемент массива равен {arr.Max}");
+            Console.WriteLine($"Сумма всех элементов массива равна {arr.Sum()}");
+            Console.WriteLine($"Сумма всех элементов массива больших 2 равна {arr.Sum(2)}");
+            int rowind, colind;
+            arr.IndexOfMaxElement(out rowind, out colind);
+            Console.WriteLine($"Индекс максимального элемента массива {rowind}, {colind}");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Чтение массива из файла data2.txt. В файле больше строк, чем указано в первой строке.");
+            arr = new Array2D("data2.txt");
+            arr.Print();
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Чтение массива из файла data3.txt. В файле меньше строк, чем указано в первой строке. Оставшиеся строки заполняются нулями.");
+            arr = new Array2D("data3.txt");
+            arr.Print();
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Попытка прочитать из несуществующего файла.");
+            try
+            {
+                arr = new Array2D("data.txt");
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Попытка прочитать из файла data4.txt. В одной из строк больше значений, чем заявленное количество колонок.");
+            try
+            {
+                arr = new Array2D("data4.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+            Console.Clear();
+            arr = new Array2D(4, 5);
+            arr.WriteToFile("result.txt");
+            Console.WriteLine("Создан массив размером 4 на 5 и записан в файл result.txt.");
+            Console.ReadKey();
+        }
+    }
+}
